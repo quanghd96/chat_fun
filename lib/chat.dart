0c9af9a7be09b446739da13cdd211118799135f6
Dang Ngoc Quang <quanghd96@gmail.com>
2018-05-11 21:59:28 +0700

    update

--- lib/chat.dart
+++ lib/chat.dart
@@ -2,8 +2,14 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
+final DatabaseReference reference =
+    FirebaseDatabase.instance.reference().child("message");
+
+const String _name = "Dang Ngoc Quang";
+var _textController = new TextEditingController();
 
 class Chat extends StatefulWidget {
   @override
@@ -12,21 +18,38 @@ class Chat extends StatefulWidget {
   }
 }
 
-class ChatState extends State<Chat> {
+class ChatState extends State<Chat> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(title: new Text("Chat")),
-        body: _buildTextComposer());
+      appBar: new AppBar(title: new Text("Chat room")),
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+              child: new FirebaseAnimatedList(
+            query: reference,
+            sort: (a, b) => b.key.compareTo(a.key),
+            padding: new EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder:
+                (_, DataSnapshot snapshot, Animation<double> animation, int n) {
+              return new ChatMessage(snapshot: snapshot, animation: animation);
+            },
+          )),
+          new Divider(height: 1.0),
+          new Container(
+            decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildTextComposer() {
-    var _textController = new TextEditingController();
     return new IconTheme(
-      //new
-      data: new IconThemeData(color: Theme.of(context).accentColor), //new
+      data: new IconThemeData(color: Theme.of(context).accentColor),
       child: new Container(
-        //modified
         margin: const EdgeInsets.symmetric(horizontal: 8.0),
         child: new Row(
           children: <Widget>[
@@ -46,9 +69,47 @@ class ChatState extends State<Chat> {
             ),
           ],
         ),
-      ), //new
+      ),
     );
   }
 
-  void _handleSubmitted(String value) {}
+  void _handleSubmitted(String value) {
+    _textController.clear();
+    reference.push().set(
+        {'text': value, 'senderName': 'Dang Ngoc Quang', 'senderAvatar': '/'});
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.snapshot, this.animation});
+
+  final Animation animation;
+
+  final DataSnapshot snapshot;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0])),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(snapshot.value['text']),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }

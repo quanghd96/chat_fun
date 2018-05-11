7d78658e1a1b4f9bfa566db45dbbfd189f7e8f55
Dang Ngoc Quang <quanghd96@gmail.com>
2018-05-11 22:20:47 +0700

    update

--- lib/chat.dart
+++ lib/chat.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
+FacebookLogin facebookLogin = new FacebookLogin();
 final DatabaseReference reference =
     FirebaseDatabase.instance.reference().child("message");
 
-const String _name = "Dang Ngoc Quang";
+String _name;
+String _avatar;
 var _textController = new TextEditingController();
 
 class Chat extends StatefulWidget {
@@ -19,6 +22,15 @@ class Chat extends StatefulWidget {
 }
 
 class ChatState extends State<Chat> with TickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    _auth.currentUser().then((FirebaseUser user) {
+      _name = user.displayName;
+      _avatar = user.photoUrl;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -75,8 +87,9 @@ class ChatState extends State<Chat> with TickerProviderStateMixin {
 
   void _handleSubmitted(String value) {
     _textController.clear();
-    reference.push().set(
-        {'text': value, 'senderName': 'Dang Ngoc Quang', 'senderAvatar': '/'});
+    reference
+        .push()
+        .set({'message': value, 'senderName': _name, 'senderAvatar': _avatar});
   }
 }
 
@@ -96,15 +109,19 @@ class ChatMessage extends StatelessWidget {
         children: <Widget>[
           new Container(
             margin: const EdgeInsets.only(right: 16.0),
-            child: new CircleAvatar(child: new Text(_name[0])),
+            child: new CircleAvatar(
+                child: new Text(snapshot.value['senderName'].length > 0
+                    ? snapshot.value['senderName'][0]
+                    : 'A')),
           ),
           new Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Text(snapshot.value['senderName'],
+                  style: Theme.of(context).textTheme.subhead),
               new Container(
                 margin: const EdgeInsets.only(top: 5.0),
-                child: new Text(snapshot.value['text']),
+                child: new Text(snapshot.value['message']),
               ),
             ],
           ),
